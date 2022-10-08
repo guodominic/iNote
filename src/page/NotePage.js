02fb872b3818cf23e9f63a4f7abc1d62e42e569d
Guo <dominic@Guos-Mac-mini.lan>
2022-10-08 00:45:04 -0400

    final final

--- src/page/NotePage.js
+++ src/page/NotePage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 //import notes from '../assets/data';
 import { useParams, useNavigate } from 'react-router-dom';
 import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
+import { ReactComponent as Addlist } from '../assets/addlist.svg';
 
 
 const NotePage = () => {
@@ -9,6 +10,7 @@ const NotePage = () => {
     const navigate = useNavigate()
     //const note = notes.find(note => note.id === Number(id))
     const [note, setNote] = useState([])
+    const [haveJoke, setHaveJoke] = useState(false)
 
 
     useEffect(() => {
@@ -30,7 +32,7 @@ const NotePage = () => {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ ...note, 'updated': new Date() })
+            body: JSON.stringify({ ...note })
         })
     }
 
@@ -51,7 +53,7 @@ const NotePage = () => {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ ...note, 'updated': new Date() })
+            body: JSON.stringify({ ...note })
         })
     }
 
@@ -66,6 +68,22 @@ const NotePage = () => {
         navigate('/iNote')
     }
 
+    const addJokeSetUp = () => {
+        fetch('https://v2.jokeapi.dev/joke/Any?')
+            .then(res => res.json())
+            .then(joke => {
+                joke.setup ?
+                    setNote(prev => ({
+                        ...note,
+                        'body': prev.body + `\n\nJoke setup: ` + joke.setup + `\nJoke: ` + joke.delivery
+                    }))
+                    : setNote(prev => ({
+                        ...note,
+                        'body': prev.body + `\nJoke: ` + joke.joke
+                    }))
+            })
+    }
+
 
     return (
         <div className='note'>
@@ -73,10 +91,22 @@ const NotePage = () => {
                 <h3>
                     <ArrowLeft onClick={handleSubmit} />
                 </h3>
-                {id === 'new' ? <button onClick={handleSubmit}>Done</button> : <button onClick={deleteNote}>Delete</button>}
+                {
+                    id === 'new' ?
+                        <button onClick={handleSubmit}>Done</button>
+                        : <button onClick={deleteNote}>Delete</button>}
             </div>
-            <textarea value={note.body ? note.body : ''} onChange={e => { setNote({ 'id': id, 'body': e.target.value, 'updated': new Date() }) }}>
+            <textarea
+                value={note.body ? note.body : ''}
+                onChange={e => {
+                    setNote({
+                        'id': id,
+                        'body': e.target.value,
+                        'updated': new Date()
+                    })
+                }}>
             </textarea>
+            <Addlist className='floating-button smaller' onClick={addJokeSetUp} />
         </div >
     )
 }
